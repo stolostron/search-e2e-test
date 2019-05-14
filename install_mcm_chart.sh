b48ac933996f1e8d2411fb1cd159537c421a8d6c
Sherin Varughese <sherin.v@ibm.com>
2019-05-14 13:29:54 -0400

    Update install_mcm_chart.sh

--- install_mcm_chart.sh
+++ install_mcm_chart.sh
@@ -6,8 +6,7 @@ pwd
 #ls -la
 #echo 'files inside stable/ibm-mcm-prod:'
 #ls -la ./stable/ibm-mcm-prod
-#cloudctl login -a https://9.30.183.233:8443 -u admin -p AHippopotamusPlaysHopscotchWithAnElephant -n kube-system
-cloudctl login -a https://9.30.183.233:8443 --skip-ssl-validation
+cloudctl login -a https://9.30.183.233:8443 -u admin -p AHippopotamusPlaysHopscotchWithAnElephant -n kube-system
 kubectl delete secret my-docker-secret
 
 kubectl create secret docker-registry -n kube-system  my-docker-secret --docker-server=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com  --docker-username=sherin.v@ibm.com --docker-password=AKCp5cbnH2sqVmttjFBmRMFqNfpVfYUYX2xPJmuxzuN77xrpqCUo8GeU5NP7ig9fAwTX42DWu
