e4cdd6c4bb1e3bb73759502b91ea30f33efc78ec
Sherin Varughese <sherin.v@ibm.com>
2019-05-14 11:43:32 -0400

    Update install_mcm_chart.sh

--- install_mcm_chart.sh
+++ install_mcm_chart.sh
@@ -1,17 +1,20 @@
 git clone git@github.ibm.com:IBMPrivateCloud/ibm-mcm-chart
 
+cd ibm-mcm-chart
+
+pwd
+ls -la
 cloudctl login -a https://9.30.183.233:8443 -u admin -p AHippopotamusPlaysHopscotchWithAnElephant -n kube-system
 
 kubectl delete secret my-docker-secret
 
 kubectl create secret docker-registry -n kube-system  my-docker-secret --docker-server=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com  --docker-username=sherin.v@ibm.com --docker-password=AKCp5cbnH2sqVmttjFBmRMFqNfpVfYUYX2xPJmuxzuN77xrpqCUo8GeU5NP7ig9fAwTX42DWu
 
-cd ibm-mcm-chart
-
 make local
+pwd
+ls -la
+#helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret repo/stable/ibm-mcm-prod-99.99.99.tgz --tls
 
-helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret repo/stable/ibm-mcm-prod-99.99.99.tgz --tls
+#cd ..
 
-cd ..
-
-kubectl get pods | grep multicluster
+#kubectl get pods | grep multicluster
