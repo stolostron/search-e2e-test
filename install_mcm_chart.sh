e932286823af07bfc802f70754b9125cea3d4216
Sherin Varughese <sherin.v@ibm.com>
2019-05-14 13:07:58 -0400

    Update install_mcm_chart.sh

--- install_mcm_chart.sh
+++ install_mcm_chart.sh
@@ -1,12 +1,11 @@
-git clone git@github.ibm.com:IBMPrivateCloud/ibm-mcm-chart
-
+#git clone git@github.ibm.com:IBMPrivateCloud/ibm-mcm-chart
+git clone --single-branch --branch temp-e2e-test git@github.com:IBMPrivateCloud/ibm-mcm-chart
 cd ibm-mcm-chart
 
 pwd
 ls -la
 echo 'files inside stable/ibm-mcm-prod:'
 ls -la ./stable/ibm-mcm-prod
-cat ./stable/ibm-mcm-prod/values.yaml
 cloudctl login -a https://9.30.183.233:8443 -u admin -p AHippopotamusPlaysHopscotchWithAnElephant -n kube-system
 
 kubectl delete secret my-docker-secret
@@ -14,6 +13,7 @@ kubectl delete secret my-docker-secret
 kubectl create secret docker-registry -n kube-system  my-docker-secret --docker-server=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com  --docker-username=sherin.v@ibm.com --docker-password=AKCp5cbnH2sqVmttjFBmRMFqNfpVfYUYX2xPJmuxzuN77xrpqCUo8GeU5NP7ig9fAwTX42DWu
 
 #make local
+
 pwd
 ls -la
 echo 'files inside stable:'
