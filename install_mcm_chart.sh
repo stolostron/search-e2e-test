d6f984843fffc27e8c99c1f4611f271a7df87f87
Sherin Varughese <sherin.v@ibm.com>
2019-05-14 15:18:31 -0400

    update scr

--- install_mcm_chart.sh
+++ install_mcm_chart.sh
@@ -1,23 +1,18 @@
-#git clone git@github.ibm.com:IBMPrivateCloud/ibm-mcm-chart
-git clone --single-branch --branch temp-e2e-test git@github.ibm.com:IBMPrivateCloud/ibm-mcm-chart
+git clone git@github.ibm.com:IBMPrivateCloud/ibm-mcm-chart
 cd ibm-mcm-chart
 
-pwd
-#ls -la
-#echo 'files inside stable/ibm-mcm-prod:'
-#ls -la ./stable/ibm-mcm-prod
 cloudctl login -a https://9.30.183.233:8443 -u admin -p ${CLOUD_PW} -n kube-system
 kubectl delete secret my-docker-secret
 
 kubectl create secret docker-registry -n kube-system  my-docker-secret --docker-server=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com  --docker-username=${DOCKER_USERNAME} --docker-password=${DOCKER_PASSWORD}
 
-make local
+# make local for mcm-chart
+LOCAL_REPO=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com/ibmcom
+for file in `find . -name values.yaml`; do echo $$file; sed -i -e "s|ibmcom|$(LOCAL_REPO)|g" $$file; done
+make charts
+for file in `find . -name values.yaml`; do echo $$file; sed -i -e "s|$(LOCAL_REPO)|ibmcom|g" $$file; done
 
-pwd
-ls -la
-echo 'files inside stable/ibm-mcm-prod:'
-ls -la ./stable/ibm-mcm-prod
-#helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret stable/ibm-mcm-prod --tls
+helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret stable/ibm-mcm-prod --tls
 
 cd ..
 
