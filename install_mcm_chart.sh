954cee94ed704eb664c801585778c157096761b7
Sherin Varughese <sherin.v@ibm.com>
2019-05-14 13:26:30 -0400

    Update install_mcm_chart.sh

--- install_mcm_chart.sh
+++ install_mcm_chart.sh
@@ -3,22 +3,22 @@ git clone --single-branch --branch temp-e2e-test git@github.ibm.com:IBMPrivateCl
 cd ibm-mcm-chart
 
 pwd
-ls -la
-echo 'files inside stable/ibm-mcm-prod:'
-ls -la ./stable/ibm-mcm-prod
-cloudctl login -a https://9.30.183.233:8443 -u admin -p AHippopotamusPlaysHopscotchWithAnElephant -n kube-system
-
+#ls -la
+#echo 'files inside stable/ibm-mcm-prod:'
+#ls -la ./stable/ibm-mcm-prod
+#cloudctl login -a https://9.30.183.233:8443 -u admin -p AHippopotamusPlaysHopscotchWithAnElephant -n kube-system
+cloudctl login -a https://9.30.183.233:8443 --skip-ssl-validation
 kubectl delete secret my-docker-secret
 
 kubectl create secret docker-registry -n kube-system  my-docker-secret --docker-server=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com  --docker-username=sherin.v@ibm.com --docker-password=AKCp5cbnH2sqVmttjFBmRMFqNfpVfYUYX2xPJmuxzuN77xrpqCUo8GeU5NP7ig9fAwTX42DWu
 
-#make local
+make local
 
 pwd
 ls -la
-echo 'files inside stable:'
+echo 'files inside stable/ibm-mcm-prod:'
 ls -la ./stable/ibm-mcm-prod
-helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret stable/ibm-mcm-prod --tls
+#helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret stable/ibm-mcm-prod --tls
 
 cd ..
 
