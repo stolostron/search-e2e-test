93a864b36b77a133141792a7eb27d96e67f50302
Sherin Varughese <sherin.v@ibm.com>
2019-05-15 13:46:55 -0400

    Update install_mcm_chart.sh

--- install_mcm_chart.sh
+++ install_mcm_chart.sh
@@ -7,7 +7,7 @@ kubectl delete secret my-docker-secret
 kubectl create secret docker-registry -n kube-system  my-docker-secret --docker-server=hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com  --docker-username=${DOCKER_USERNAME} --docker-password=${DOCKER_PASSWORD}
 
 # make local for mcm-chart
-for file in `find . -name values.yaml`; do echo $$file; sed -i -e "s|ibmcom|hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com/ibmcom|g" $$file; done
+for file in `find . -name values.yaml`; do echo $file; sed -i -e "s|ibmcom|hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com/ibmcom|g" $file; done
 make charts
 
 helm upgrade --install multicluster-hub --namespace kube-system --set compliance.mcmNamespace=mcm --set global.pullSecret=my-docker-secret repo/stable/ibm-mcm-prod-99.99.99.tgz --tls
