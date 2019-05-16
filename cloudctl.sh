785e14c74267579b517b304e9c3d0a9877cb8ef8
Sherin Varughese <sherin.v@ibm.com>
2019-05-16 13:01:17 -0400

    helm init2

--- cloudctl.sh
+++ cloudctl.sh
@@ -7,11 +7,18 @@ chmod 755 kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 #cp ~/.kube/noted-skunk-icp-cluster/*.pem ~/.helm/
+curl -kLo helm-linux-amd64.tar.gz https://9.30.183.233:8443/api/cli/helm-linux-amd64.tar.gz
+mkdir helm-unpacked
+tar -xvzf helm-linux-amd64.tar.gz -C helm-unpacked
+chmod 755 ./helm-unpacked/*/helm
+sudo mv ./helm-unpacked/*/helm /usr/local/bin/helm
+rm -rf ./helm-unpacked ./helm-linux-amd64.tar.gz
+helm init
 
 
 cloudctl login -a https://9.30.183.233:8443 -u admin -p ${CLOUD_PW} -n kube-system
 echo 'check1'
 kubectl version
-helm init
 helm version --tls
+echo 'check2'
 echo 'bye'
