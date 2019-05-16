d6339ab28444bbfcb93f4803f69f6a7731cd6759
Sherin Varughese <sherin.v@ibm.com>
2019-05-16 12:55:07 -0400

    init helm

--- cloudctl.sh
+++ cloudctl.sh
@@ -12,5 +12,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 cloudctl login -a https://9.30.183.233:8443 -u admin -p ${CLOUD_PW} -n kube-system
 echo 'check1'
 kubectl version
+helm init
 helm version --tls
 echo 'bye'
