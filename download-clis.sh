453616201b1ebf4a295df0da0906d0846f1c5688
Jorge Padilla <jpadilla@redhat.com>
2020-05-28 23:24:30 -0400

    Update download-clis.sh

--- download-clis.sh
+++ download-clis.sh
@@ -1,7 +1,8 @@
 # Install OpenShift CLI.
-curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.3/linux/oc.tar.gz
+echo "Installing oc CLI..."
+curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.4/linux/oc.tar.gz
 mkdir oc-unpacked
-tar -xvzf oc.tar.gz -C oc-unpacked
+tar -xzf oc.tar.gz -C oc-unpacked
 chmod 755 ./oc-unpacked/oc
 mv ./oc-unpacked/oc /usr/local/bin/oc
 rm -rf ./oc-unpacked ./oc.tar.gz
