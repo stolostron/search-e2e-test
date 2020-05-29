7f60cdc9d126f6e7dd706ad145b4f8f7c32c5946
Jorge Padilla <jpadilla@redhat.com>
2020-05-28 22:29:38 -0400

    Cleanup Dockerization

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
-FROM golang:1.14 as builder
-# get yq
-RUN go get github.com/mikefarah/yq
+# Copyright (c) 2020 Red Hat, Inc.
 
 FROM node:12.16.3-stretch as production
 
 USER root
-# Install yq
-COPY --from=builder /go/bin/yq /usr/bin/yq
 
 # Install Firefox
 RUN apt-get update && apt-get install -y firefox-esr
@@ -14,18 +10,10 @@ RUN apt-get update && apt-get install -y firefox-esr
 RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
 
-# Install sudo
-RUN apt-get install -y sudo
+WORKDIR .
+ADD . .
 
-WORKDIR /usr/src/app
-
-#Add source files
-RUN mkdir -p /opt/app-root/search-e2e
-WORKDIR /opt/app-root/search-e2e
-COPY . /opt/app-root/search-e2e
-
-RUN chmod u+x ./download-clis.sh && ./download-clis.sh
+RUN sh download-clis.sh
 RUN npm install
-RUN chmod u+x ./start.sh
 
-CMD ["./start.sh"]
+CMD ["npm", "run", "test:e2e-headless"]
--- config/index.js
+++ config/index.js
@@ -5,24 +5,38 @@
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
  * Contract with IBM Corp.
+ * Copyright (c) 2020 Red Hat, Inc.
  *******************************************************************************/
 var nconf = require('nconf'),
-    log4js = require('log4js'),
-    logger = log4js.getLogger('server'),
+    nconfYaml = require('nconf-yaml'),
     path = require('path')
 
 var configDir = path.resolve(__dirname)
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-process.env.NODE_ENV = process.env.BUILD_ENV || process.env.NODE_ENV
+nconf.env({ lowerCase: true, separator: '_' })
+    .file({file: './options.yaml', format: nconfYaml })
+    .defaults({
+        CLUSTER_PORT: '443',
+        CLUSTER_VIEWER_USR: 'user-viewer',
+        CLUSTER_VIEWER_PWD: 'pass-viewer',
+        contextPath: '/multicloud'
+    })
 
-nconf.argv().env('__')
+nconf.required(['options:hub:baseDomain', 'options:hub:user', 'options:hub:password'])
 
-var env = nconf.get('NODE_ENV')
+if (nconf.get('options:hub:baseDomain') === ''){
+    console.error('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    console.error('OPTIONS_HUB_BASEDOMAIN is a required env variable, but got empty or undefined.')
+    console.error('Exiting tests...')
+    console.error('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
+    process.exit(1)
+  }
 
-logger.info(`NODE_ENV=${process.env.NODE_ENV}`)
-
-nconf.file(env, `${configDir}/config-${env}.json`)
-nconf.file('default', `${configDir}/config-defaults.json`)
+console.log('Test environment')
+console.log('========================================')
+console.log('baseDomain: ', nconf.get('options:hub:baseDomain'))
+console.log('user      : ', nconf.get('options:hub:user'))
+console.log('password  : ', nconf.get('options:hub:password'))
+console.log('========================================\n')
 
 module.exports = nconf
