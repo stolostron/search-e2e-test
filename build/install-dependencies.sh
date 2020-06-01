946ea535c61d61abb8e4024a9f0b62cea1c685e7
Kyl-Bempah <kbempah@redhat.com>
2020-06-01 13:32:53 -0400

    Acd install dependencies

---
+++ build/install-dependencies.sh
@@ -0,0 +1,10 @@
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
+# Copyright (c) 2020 Red Hat, Inc
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+
+#!/bin/bash
+set -e
+exit 0
