97b5ba57ad39b89c0fa679068b6c9d980ee8c0ed
Kyl-Bempah <kbempah@redhat.com>
2020-06-02 10:50:45 -0400

    Update based on comments

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,6 @@
-# Licensed Materials - Property of IBM
-# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
 # Copyright (c) 2020 Red Hat, Inc
 # Note to U.S. Government Users Restricted Rights:
 # Use, duplication or disclosure restricted by GSA ADP Schedule
-# Contract with IBM Corp.
 
 #!/bin/bash
 set -e
