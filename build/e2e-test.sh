d54df3dd08d99963f0e97179aaa5f788cac3940f
rhowingt@redhat.com <rhowingt@redhat.com>
2020-07-29 14:51:58 -0400

    remove oclogin from e2e-test

--- build/e2e-test.sh
+++ build/e2e-test.sh
@@ -6,11 +6,4 @@
 
 set -e
 
-# export OC_CLUSTER_URL=$OPTIONS_HUB_BASEDOMAIN
-# export OC_CLUSTER_USER=$OPTIONS_HUB_USER
-# export OC_CLUSTER_PASS=$OPTIONS_HUB_PASSWORD
-
-# make oc/install
-# make oc/login
-
 make run-test-image-pr
