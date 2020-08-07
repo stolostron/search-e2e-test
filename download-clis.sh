50c97d0254cfa621f84d8abad30bbedcd2b74864
rhowingt@redhat.com <rhowingt@redhat.com>
2020-08-07 01:20:53 -0400

    remove docker-cli download

--- download-clis.sh
+++ download-clis.sh
@@ -13,29 +13,4 @@ chmod 755 ./clis-unpacked/kubectl
 mv ./clis-unpacked/oc /usr/local/bin/oc
 mv ./clis-unpacked/kubectl /usr/local/bin/kubectl
 
-# Install Docker CLI
-echo "Installing docker cli..."
-curl -kLo docker.tgz https://download.docker.com/linux/static/stable/x86_64/docker-18.06.0-ce.tgz
-tar -xzf docker.tgz -C clis-unpacked
-chmod 755 ./clis-unpacked/docker
-mv ./clis-unpacked/docker/* /usr/local/bin/
-
-rm -rf ./clis-unpacked ./oc.tar.gz ./docker.tgz
-
-# oc login --server=${OCP_SERVER} -u ${OCP_CONSOLE_USR} -p ${OCP_CONSOLE_PWD} --insecure-skip-tls-verify=true
-
-# Install helm CLI.
-# curl -kLo helm-linux-amd64.tar.gz https://${CLUSTER_IP}:${CLUSTER_PORT}/api/cli/helm-linux-amd64.tar.gz
-# mkdir helm-unpacked
-# tar -xvzf helm-linux-amd64.tar.gz -C helm-unpacked
-# chmod 755 ./helm-unpacked/*/helm
-# sudo mv ./helm-unpacked/*/helm /usr/local/bin/helm
-# rm -rf ./helm-unpacked ./helm-linux-amd64.tar.gz
-# helm init
-
-# Setup helm certs
-# oc get secret helm-tiller-secret -n kube-system -o json | jq -r .data.crt | base64 --decode > ~/.helm/cert.pem
-# oc get secret helm-tiller-secret -n kube-system -o json | jq -r .data.key | base64 --decode > ~/.helm/key.pem
-
-# helm version --tls
 echo 'set up complete'
