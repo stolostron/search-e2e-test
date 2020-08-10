ee4c555861f648a0468024095845ce37350024b5
Jorge Padilla <jpadilla@redhat.com>
2020-08-10 13:31:50 -0400

    Update copyrights

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,8 +1,7 @@
-# Copyright (c) 2020 Red Hat, Inc
-# Note to U.S. Government Users Restricted Rights:
-# Use, duplication or disclosure restricted by GSA ADP Schedule
-
 #!/bin/bash
+
+# Copyright (c) 2020 Red Hat, Inc
+
 set -e
 
 make install
