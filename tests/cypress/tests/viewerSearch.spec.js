3aadc92aa9e1c0bf2c40996ee2554ee6e9a97176
rhowingt@redhat.com <rhowingt@redhat.com>
2020-08-11 23:03:22 -0400

    rm /xml dir, update viewersearch test env. vars

--- tests/cypress/tests/viewerSearch.spec.js
+++ tests/cypress/tests/viewerSearch.spec.js
@@ -5,13 +5,13 @@
 
 /// <reference types="cypress" />
 
-import { pageLoader, searchPage, searchBar } from '../views/search'
+import { pageLoader, searchPage } from '../views/search'
 
 describe('Login', {
   env: {
     OC_IDP: 'search-e2e',
-    OPTIONS_HUB_USER: 'user-viewer',
-    OPTIONS_HUB_PASSWORD : 'pass-viewer'
+    CYPRESS_OPTIONS_HUB_USER: 'user-viewer',
+    CYPRESS_OPTIONS_HUB_PASSWORD : 'pass-viewer'
   }
 },() => {
   it('page should load', () => {
