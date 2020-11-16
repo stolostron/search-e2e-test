f582f45639ce66692c63d345c3d55f16e6767ce2
Jorge Padilla <jpadilla@redhat.com>
2020-11-16 16:06:22 -0500

    Filter console-ui-test clusters

--- tests/cypress/views/clusters.js
+++ tests/cypress/views/clusters.js
@@ -11,7 +11,7 @@ export const clustersPage = {
 
     return cy.contains('table.resource-table th', 'Name', {timeout: 6000})
              .invoke('index')
-             .then((index) => cy.get('table.resource-table tbody tr').filter(':not(:contains("local-cluster"))').eq(0).get('td').eq(index).invoke('text'));
+             .then((index) => cy.get('table.resource-table tbody tr').filter(':not(:contains("local-cluster"))').filter(':not(:contains("console-ui-test-"))').eq(0).get('td').eq(index).invoke('text'));
   },
   shouldExist: () => {
     cy.get('.bx--detail-page-header-title').should('contain', 'Clusters')
