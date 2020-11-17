fd007c65c58611d631b8fa156d64e7082a587b40
Jorge Padilla <jpadilla@redhat.com>
2020-11-17 13:20:11 -0500

    Suggested searches test was executed twice (#55)
    * Suggested searches test was executed twice
    
    * Change order

--- tests/cypress/tests/adminSearch.spec.js
+++ tests/cypress/tests/adminSearch.spec.js
@@ -124,39 +124,43 @@ clusterModes.forEach((clusterMode) =>   {
       });
     })
   })
-  describe('Verify the suggested search templates', function() {
-
-    before(function() {
-      cy.login()
-    })
-  
-    after(function() {
-      cy.logout()
-    })
-    beforeEach(function() {
-      searchPage.whenGoToSearchPage()
-      
-    })
-    it('should see the created last hour template & search tag in search items', function() {
-      suggestedTemplate.whenSelectCreatesLastHour()
-    });
-    it('should see the workloads template & search tag in search items', function() {
-      suggestedTemplate.whenSelectWorkloads()
-    });
-    it('should see the unhealthy pods template & search tag in search items', function() {
-      suggestedTemplate.whenSelectUnhealthyPods()
-    });
-    it('should see the created last hour template & related items details', function() {
-      suggestedTemplate.whenSelectCreatesLastHour()
-      suggestedTemplate.whenVerifyRelatedItemsDetails()
-    });
-    it('should see the workload template & related items details', function() {
-      suggestedTemplate.whenSelectWorkloads()
-      suggestedTemplate.whenVerifyRelatedItemsDetails()
-    });
-    it('should see the unhealthy pods template & related items details', function() {
-      suggestedTemplate.whenSelectUnhealthyPods()
-      suggestedTemplate.whenVerifyRelatedItemsDetails()
-    });
-  })
 });
+
+describe('Verify the suggested search templates', function() {
+
+  before(function() {
+    cy.login()
+  })
+
+  after(function() {
+    cy.logout()
+  })
+  beforeEach(function() {
+    searchPage.whenGoToSearchPage()
+    
+  })
+
+  it('should see the workloads template & search tag in search items', function() {
+    suggestedTemplate.whenSelectWorkloads()
+  });
+  it('should see the unhealthy pods template & search tag in search items', function() {
+    suggestedTemplate.whenSelectUnhealthyPods()
+  });
+  it('should see the created last hour template & search tag in search items', function() {
+    suggestedTemplate.whenSelectCreatesLastHour()
+  });
+
+  // Verify the related resources.
+  it('should see the workload template & related items details', function() {
+    suggestedTemplate.whenSelectWorkloads()
+    suggestedTemplate.whenVerifyRelatedItemsDetails()
+  });
+  it('should see the unhealthy pods template & related items details', function() {
+    suggestedTemplate.whenSelectUnhealthyPods()
+    suggestedTemplate.whenVerifyRelatedItemsDetails()
+  });
+  it('should see the created last hour template & related items details', function() {
+    suggestedTemplate.whenSelectCreatesLastHour()
+    suggestedTemplate.whenVerifyRelatedItemsDetails()
+  });
+})
