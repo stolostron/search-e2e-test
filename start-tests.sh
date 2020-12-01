bfb424cbf626eed134b9bea6f675cd945bea9c30
Jorge Padilla <jpadilla@redhat.com>
2020-11-30 19:10:31 -0500

    Update start-tests.sh

--- start-tests.sh
+++ start-tests.sh
@@ -56,8 +56,7 @@ echo "Running Search API tests."
 npm run test:api
 
 
-echo "\n\n"
-echo "Running Search UI tests."
+echo "\n\nRunning Search UI tests."
 if [ "$NODE_ENV" == "dev" ]; then
   npx cypress run --browser $BROWSER $HEADLESS --spec ./tests/cypress/tests/*.spec.js --reporter cypress-multi-reporters  
 elif [ "$NODE_ENV" == "debug" ]; then
