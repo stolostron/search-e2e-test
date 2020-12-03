fd483415642206df5e8f3dfb2dc9b81e39ba575d
Jorge Padilla <jpadilla@redhat.com>
2020-12-03 02:45:41 -0500

    simplify

--- build/upload-to-s3.sh
+++ build/upload-to-s3.sh
@@ -4,20 +4,15 @@
 
 
 function install_aws_cli() {
-    my_dir=$(dirname $(readlink -f $0))
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip -q -o awscliv2.zip
-    ./aws/install -i $my_dir/aws-cli -b /home/travis/bin
-
-    # Setup AWS credentials.
-    # echo "aws_access_key_id = " $AWS_ACCESS_KEY_ID > ./aws/credentials
-    # echo "aws_secret_access_key = " $AWS_SECRET_ACCESS_KEY >> ./aws/credentials
+    ./aws/install -i ./aws-cli -b /home/travis/bin
 }
 
 # copies the test results to the search-e2e-test S3 bucket
 function upload_s3() {
+    install_aws_cli
     echo "Uploading files to AWS S3 bucket.  search-e2e-test/travis-${TRAVIS_BUILD_ID}"  
 
     aws s3 sync ./search-test-results s3://search-e2e-results/travis-${TRAVIS_BUILD_ID}
-    set +x
 }
