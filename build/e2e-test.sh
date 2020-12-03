1c94d3d7d73f03dabc98784908d260050124548b
Jorge Padilla <jpadilla@redhat.com>
2020-12-03 01:41:37 -0500

    Update e2e-test.sh

--- build/e2e-test.sh
+++ build/e2e-test.sh
@@ -19,7 +19,7 @@ make run-test-image-pr
 fold_end cypress
 
 
-fold_start results "Upload results to S3 bucket search-e2e-results"
+fold_start results "Upload test results to AWS S3 bucket: search-e2e-tests/travis-${TRAVIS_BUILD_ID}"
 source ./build/post-to-s3.sh
 install_aws_cli
 post_s3
