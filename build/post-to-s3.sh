baed71e7f4568325edea6adab6fd1792b0ae948b
Jorge Padilla <jpadilla@redhat.com>
2020-12-03 01:05:24 -0500

    Update post-to-s3.sh

--- build/post-to-s3.sh
+++ build/post-to-s3.sh
@@ -2,10 +2,9 @@
 
 # Copyright (c) 2020 Red Hat, Inc
 
-my_dir=$(dirname $(readlink -f $0))
 
 function install_aws_cli() {
-    echo "my_dir: " $my_dir
+    my_dir=$(dirname $(readlink -f $0))
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip -q -o awscliv2.zip
     ./aws/install -i $my_dir/aws-cli -b /home/travis/bin
@@ -13,13 +12,12 @@ function install_aws_cli() {
     echo "Setup AWS credentials"
     echo "aws_access_key_id = " $AWS_ACCESS_KEY_ID > ./aws/credentials
     echo "aws_secret_access_key = " $AWS_SECRET_ACCESS_KEY >> ./aws/credentials
-  
 }
 
+# copies the test results to the search-e2e-test/data S3 bucket
 function post_s3() {
-    echo "Uploading files to AWS S3 bucket"  
+    echo "Uploading files to AWS S3 bucket.  search-e2e-test/data/${TRAVIS_BUILD_ID}"  
 
-    # copies results to the search-e2e-test/data S3 bucket
     aws s3 sync ./search-test-results s3://search-e2e-results/data/${TRAVIS_BUILD_ID}
     set +x
 }
