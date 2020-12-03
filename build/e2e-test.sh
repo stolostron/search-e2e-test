034f8ae614c01e96ddbff7365d5ad92cf7147461
Jorge Padilla <jpadilla@redhat.com>
2020-12-03 01:52:05 -0500

    cleanup

--- build/e2e-test.sh
+++ build/e2e-test.sh
@@ -4,23 +4,9 @@
 
 set -e
 
-export UI_CURRENT_IMAGE=$1
-
-function fold_start() {
-  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
-}
-
-function fold_end() {
-  echo -e "\ntravis_fold:end:$1\r"
-}
-
-fold_start cypress "Execute Search Tests"
 make run-test-image-pr
-fold_end cypress
 
-
-fold_start results "Upload test results to AWS S3 bucket: search-e2e-tests/travis-${TRAVIS_BUILD_ID}"
+echo "Upload test results to AWS S3 bucket: search-e2e-tests/travis-${TRAVIS_BUILD_ID}"
 source ./build/post-to-s3.sh
 install_aws_cli
 post_s3
-fold_end results
