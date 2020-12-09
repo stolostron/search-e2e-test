699b35f52c348e034053b568b9d8aec590af959f
Jad Haj Yahya <jhajyahy@redhat.com>
2020-12-09 15:17:15 +0200

    ACM-529 - Adding tests RHACM4K-912

--- tests/api/access.test.js
+++ tests/api/access.test.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2020 Red Hat, Inc.
 
-const { getSearchApiRoute, getToken } = require('../common-lib/clusterAccess')
+const { getSearchApiRoute, getToken, getManagedClusterName } = require('../common-lib/clusterAccess')
 const request = require('supertest');
 const config = require('../../config');
-const { execSync } = require('child_process');
+const queries = require('../common-lib/queries.json')
 
 var searchApiRoute = ''
 var token = ''
@@ -19,7 +19,6 @@ const query = {
     },
     query: 'query searchResult($input: [SearchInput]) {\n  searchResult: search(input: $input) {\n    items\n    __typename\n  }\n}\n'
 }
-
 
 describe('Search: Verify access to the search-api', () => {
 
@@ -61,4 +60,58 @@ describe('Search: Verify access to the search-api', () => {
             .expect(200)
     }, 20000) // Timeout is high at 20 seconds because first search takes longer to build the rbac filter cache.
 
+})
+
+describe('RHACM4K-912 - Search - verify managed cluster info in the search page', () => {
+
+    beforeAll(async () => {
+        // Log in and get access token
+        token = getToken()
+
+        // Create a route to access the Search API.
+        searchApiRoute = await getSearchApiRoute()
+
+        // Temporary workaround. TODO: Get SSL cert from cluster.
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+    })
+
+    // Cleanup and teardown here.
+    afterAll(() => {
+    })
+
+    test.only('RHACM4K-912 - Search - verify managed cluster info in the search page.', async () => {
+        const res = await request(searchApiRoute)
+            .post('/searchapi/graphql')
+            .send(queries.managedClusterJoined)
+            .set({ Authorization: `Bearer ${token}` })
+            .expect(200)
+        expect(res.body.data.searchResult[0].items[0].ManagedClusterJoined).toEqual("True")
+        expect(res.body.data.searchResult[0].items[0].status).toEqual("OK")
+
+        const res1 = await request(searchApiRoute)
+            .post('/searchapi/graphql')
+            .send(queries.cluster)
+            .set({ Authorization: `Bearer ${token}` })
+            .expect(200)
+        expect(res1.body.data.searchResult[0].items[0].kind).toEqual("cluster")
+        expect(res1.body.data.searchResult[0].items[0].name).toEqual("local-cluster")
+
+        const res2 = await request(searchApiRoute)
+            .post('/searchapi/graphql')
+            .send(queries.pod)
+            .set({ Authorization: `Bearer ${token}` })
+            .expect(200)
+        expect(res2.body.data.searchResult[0].items[0].kind).toEqual("pod")
+
+        const res3 = await request(searchApiRoute)
+            .post('/searchapi/graphql')
+            .send(queries.managedClusterAgents)
+            .set({ Authorization: `Bearer ${token}` })
+            .expect(200)
+        
+        var pods = res3.body.data.searchResult[0].items
+        pods.forEach(element => {
+            expect(element.status).toEqual("Running")
+        });
+        }, 20000)
 })
