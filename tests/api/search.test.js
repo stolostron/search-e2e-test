e2acc4bf5fda9c6adb4893368de547d283bf3fb7
jadhaj <34447576+jadhaj@users.noreply.github.com>
2021-01-05 18:24:15 +0200

    Adding search api test RHACM4K-1696 (#67)

--- tests/api/search.test.js
+++ tests/api/search.test.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Red Hat, Inc.
 
-const { getSearchApiRoute, getToken, searchQueryBuilder, sendRequest } = require('../common-lib/clusterAccess')
+const { getSearchApiRoute, getToken, searchQueryBuilder, sendRequest, getPods, deletePod } = require('../common-lib/clusterAccess')
 
 describe('RHACM4K-912 - Search - verify managed cluster info in the search page', () => {
 
@@ -19,7 +19,7 @@ describe('RHACM4K-912 - Search - verify managed cluster info in the search page'
   afterAll(() => {
   })
 
-  test('RHACM4K-912 - Search - verify managed cluster info in the search page.', async () => {
+  test('RHACM4K-1695 - Search - verify managed cluster info in the search page.', async () => {
     var query = searchQueryBuilder({ filters: [{ property: 'ManagedClusterJoined', values: ['True'] }] })
     var res = await sendRequest(query, token)
     expect(res.body.data.searchResult[0].items[0].ManagedClusterJoined).toEqual("True")
@@ -43,5 +43,72 @@ describe('RHACM4K-912 - Search - verify managed cluster info in the search page'
     pods.forEach(element => {
       expect(element.status).toEqual("Running")
     })
+  }, 20000)
+})
+
+describe('RHACM4K-1696 - Search - Verify search result with common filter and conditions', () => {
+  const app = 'console'
+  const namespace = 'openshift-console'
+
+  test('Verify a deleted pod is recreated.', async () => {
+    var query = searchQueryBuilder({ filters: [{ property: 'kind', values: ['deployment'] },
+                                               { property: 'name', values: [app] },
+                                               { property: 'namespace', values: [namespace] }] })
+    var res = await sendRequest(query, token)
+    expect(res.body.data.searchResult[0].items[0].available).toEqual(2)
+    var pods = getPods(namespace)
+    deletePod(pods[0][0], namespace).then(() => {
+      var res = sendRequest(query, token)
+      expect(res.body.data.searchResult[0].items[0].available).toEqual(2)
+
+    })
+  }, 20000)
+
+  test('Search kind application on specific namespace.', async () => {
+    var query = searchQueryBuilder({ filters: [{ property: 'kind', values: ['deployment'] },
+                                               { property: 'name', values: [app] },
+                                               { property: 'namespace', values: [namespace] }] })
+    var res = await sendRequest(query, token)
+    expect(res.body.data.searchResult[0].items[0].name).toEqual(app)
+    expect(res.body.data.searchResult[0].items[0].kind).toEqual("deployment")
+    expect(res.body.data.searchResult[0].items[0].namespace).toEqual(namespace)
+  }, 20000)
+
+  test('Search kind pod and namespace open-cluster-management.', async () => {
+    var query = searchQueryBuilder({ filters: [{ property: 'kind', values: ['pod'] },
+                                               { property: 'namespace', values: ['open-cluster-management'] },
+                                               { property: 'status', values: ['Running'] }] })
+    var res = await sendRequest(query, token)
+    var pods = res.body.data.searchResult[0].items
+    pods.forEach(element => {
+      expect(element.status).toEqual("Running")
+    })
+  }, 20000)
+
+  test('Search kind pod on specific cluster.', async () => {
+    var query = searchQueryBuilder({ filters: [{ property: 'kind', values: ['pod'] },
+                                               { property: 'cluster', values: ['local-cluster'] },
+                                               { property: 'status', values: ['Running'] }] })
+    var res = await sendRequest(query, token)
+    var pods = res.body.data.searchResult[0].items
+    pods.forEach(element => {
+      expect(element.status).toEqual("Running")
+    })
+  }, 20000)
+
+  test('Search kind:certpolicycontroller.', async () => {
+    var query = searchQueryBuilder({ filters: [{ property: 'kind', values: ['certpolicycontroller'] }] })
+    var res = await sendRequest(query, token)
+    expect(res.body.data.searchResult[0].items[0].name).toEqual('klusterlet-addon-certpolicyctrl')
+    expect(res.body.data.searchResult[0].items[0].kind).toEqual("certpolicycontroller")
+    expect(res.body.data.searchResult[0].items[0].namespace).toEqual('open-cluster-management-agent-addon')
+  }, 20000)
+
+  test('Search kind:iampolicycontroller.', async () => {
+    var query = searchQueryBuilder({ filters: [{ property: 'kind', values: ['iampolicycontroller'] }] })
+    var res = await sendRequest(query, token)
+    expect(res.body.data.searchResult[0].items[0].name).toEqual('klusterlet-addon-iampolicyctrl')
+    expect(res.body.data.searchResult[0].items[0].kind).toEqual("iampolicycontroller")
+    expect(res.body.data.searchResult[0].items[0].namespace).toEqual('open-cluster-management-agent-addon')
   }, 20000)
 })
