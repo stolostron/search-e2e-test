9af5302b3526acb8a6f806aa20155efd1c357fb3
dislbenn <lavontae.bennett@gmail.com>
2021-01-15 13:41:58 -0500

    updated saved searches test

--- tests/cypress/tests/savedSearches.spec.js
+++ tests/cypress/tests/savedSearches.spec.js
@@ -4,16 +4,16 @@
  ****************************************************************************** */
 
 /// <reference types="cypress" />
-import { clusterNamespace } from "../views/clusterNamespaceSearch"
-import { squad } from "../config"
+import { clusterNamespace } from '../views/savedSearches'
+import { squad } from '../config'
 
-const queryDefaultNamespaceName = "default namespace search"
-const queryDefaultNamespaceDesc = "this is searching that each cluster should have default namespace"
+const queryDefaultNamespaceName = 'default namespace search'
+const queryDefaultNamespaceDesc = 'this is searching that each cluster should have default namespace'
 
-const queryOcmaNamespaceName = "open-cluster-management-agent search"
-const queryOcmaNamespaceDesc = "this is searching that each cluster should have open-cluster-management-agent"
+const queryOcmaNamespaceName = 'open-cluster-management-agent search'
+const queryOcmaNamespaceDesc = 'this is searching that each cluster should have open-cluster-management-agent'
 
-describe("Search: Saved searches", function(){
+describe('Search: Saved searches', function(){
 
   before(function() {
     cy.login()
@@ -26,16 +26,16 @@ describe("Search: Saved searches", function(){
   })
 
   it(`[P2][Sev2][${squad}] should find each managed cluster has default namespace`, function() {
-    clusterNamespace.validateClusterNamespace({"namespace": "default"}, "")
+    clusterNamespace.validateClusterNamespace({'namespace': 'default'}, '')
   })
 
   it(`[P2][Sev2][${squad}] should find open-cluster-management-agent namespace exists`, function() {
-    clusterNamespace.validateClusterNamespace({"kind": "namespace","name" : "open-cluster-management-agent" }, "has_local-cluster")
+    clusterNamespace.validateClusterNamespace({'kind': 'namespace','name' : 'open-cluster-management-agent' }, 'has_local-cluster')
   })
 
   it(`[P2][Sev2][${squad}] should be able to save current search`, function(){
-    clusterNamespace.saveClusterNamespaceSearch({"namespace": "default" }, queryDefaultNamespaceName, queryDefaultNamespaceDesc)
-    clusterNamespace.saveClusterNamespaceSearch({"kind": "namespace","name" : "open-cluster-management-agent" }, queryOcmaNamespaceName, queryOcmaNamespaceDesc)
+    clusterNamespace.saveClusterNamespaceSearch({'namespace': 'default' }, queryDefaultNamespaceName, queryDefaultNamespaceDesc)
+    clusterNamespace.saveClusterNamespaceSearch({'kind': 'namespace','name' : 'open-cluster-management-agent' }, queryOcmaNamespaceName, queryOcmaNamespaceDesc)
   })
 
   it(`[P2][Sev2][${squad}] should be able to find the saved searches`, function() {
