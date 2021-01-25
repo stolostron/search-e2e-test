c1fe534dc54754c7e21cc8ee35a7996490719df6
Jorge Padilla <jpadilla@redhat.com>
2021-01-25 17:03:07 -0500

    Fix s3 upload

--- build/upload-to-s3.sh
+++ build/upload-to-s3.sh
@@ -7,6 +7,9 @@ function install_aws_cli() {
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip -q -o awscliv2.zip
     ./aws/install -i ./aws-cli -b /home/travis/bin
+
+    echo "Validate AWS cli install running [ /home/travis/bin/aws --version ]"
+    /home/travis/bin/aws --version
 }
 
 # copies the test results to the search-e2e-test S3 bucket
@@ -14,5 +17,5 @@ function upload_s3() {
     install_aws_cli
     echo "Uploading files to AWS S3 bucket.  search-e2e-test/travis-${TRAVIS_BUILD_ID}"  
 
-    aws s3 sync ./search-test-results s3://search-e2e-results/travis-${TRAVIS_BUILD_ID}
+    /home/travis/bin/aws s3 sync ./search-test-results s3://search-e2e-results/travis-${TRAVIS_BUILD_ID}
 }
