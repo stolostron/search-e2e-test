7d73401897335050a60458ba646fe6af76260d38
Jorge Padilla <jpadilla@redhat.com>
2021-01-25 23:55:55 -0500

    Update e2e-test.sh

--- build/e2e-test.sh
+++ build/e2e-test.sh
@@ -6,10 +6,8 @@
 make run-test-image-pr
 
 echo "Uploading test results to AWS S3 bucket."
-echo "e2e-test.sh >>> pwd:"
-pwd
 source ./build/upload-to-s3.sh
-sudo install_aws_cli
-sudo upload_s3
+install_aws_cli
+upload_s3
 
 echo "Test results uploaded to: https://s3.console.aws.amazon.com/s3/buckets/search-e2e-results?region=us-east-1&prefix=travis-${TRAVIS_BUILD_ID}/&showversions=false"
