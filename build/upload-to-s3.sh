63f523fbb39314ba57310e7fdd1ee6476b059eac
Jorge Padilla <jpadilla@redhat.com>
2021-01-25 22:21:55 -0500

    Update upload-to-s3.sh

--- build/upload-to-s3.sh
+++ build/upload-to-s3.sh
@@ -22,5 +22,5 @@ function upload_s3() {
     install_aws_cli
     echo "Uploading files to AWS S3 bucket.  search-e2e-test/travis-${TRAVIS_BUILD_ID}"  
 
-    sudo ./home/travis/bin/aws s3 sync ./search-test-results s3://search-e2e-results/travis-${TRAVIS_BUILD_ID}
+    sudo home/travis/bin/aws s3 sync ./search-test-results s3://search-e2e-results/travis-${TRAVIS_BUILD_ID}
 }
