dfa57874523d81db41ccb344fcfcaa5d5c3b2364
Jorge Padilla <jpadilla@redhat.com>
2021-01-26 00:40:33 -0500

    :fire: finalize

--- build/upload-to-s3.sh
+++ build/upload-to-s3.sh
@@ -7,17 +7,7 @@ function install_aws_cli() {
     aws_dir=$(dirname $(readlink -f $0))
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip -q -o awscliv2.zip
-    sudo ./aws/install -i $aws_dir/aws-cli -b /home/travis/bin
-
-    echo "Validate AWS cli install running [ sudo /home/travis/bin/aws --version ]"
-    $aws_dir/aws-cli/v2/current/bin/aws --version
-    echo ">>> aws --version"
-    aws --version
-    echo " >> ls ./aws-cli/v2/current/bin"
-    ls $aws_dir/aws-cli/v2/current/bin
-
-    echo "Printing PATH"
-    echo $PATH
+    ./aws/install -i $aws_dir/aws-cli -b /home/travis/bin
 }
 
 # copies the test results to the search-e2e-test S3 bucket
