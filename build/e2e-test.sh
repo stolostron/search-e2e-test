74b062c54ce05290aead95571502d21317db00de
Jorge Padilla <jpadilla@redhat.com>
2021-01-25 22:30:43 -0500

    s3 upload

--- build/e2e-test.sh
+++ build/e2e-test.sh
@@ -6,7 +6,10 @@
 make run-test-image-pr
 
 echo "Uploading test results to AWS S3 bucket."
-source ./build/upload-to-s3.sh
-upload_s3
+echo "e2e-test.sh >>> pwd:"
+pwd
+sudo source ./build/upload-to-s3.sh
+sudo install_aws_cli
+sudo upload_s3
 
 echo "Test results uploaded to: https://s3.console.aws.amazon.com/s3/buckets/search-e2e-results?region=us-east-1&prefix=travis-${TRAVIS_BUILD_ID}/&showversions=false"
--- build/upload-to-s3.sh
+++ build/upload-to-s3.sh
@@ -14,12 +14,10 @@ function install_aws_cli() {
     ls aws-cli/v2/current
     echo " >> ls -la /home/travis/bin"
     ls -la /home/travis/bin
-
 }
 
 # copies the test results to the search-e2e-test S3 bucket
 function upload_s3() {
-    install_aws_cli
     echo "Uploading files to AWS S3 bucket.  search-e2e-test/travis-${TRAVIS_BUILD_ID}"  
 
     sudo home/travis/bin/aws s3 sync ./search-test-results s3://search-e2e-results/travis-${TRAVIS_BUILD_ID}
