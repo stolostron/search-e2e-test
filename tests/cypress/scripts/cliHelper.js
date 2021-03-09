f27d1ba646cb563ba18808412a0f38f95b9a45d5
dislbenn <lavontae.bennett@gmail.com>
2021-03-09 11:12:43 -0500

    updated cliHelper.js

--- tests/cypress/scripts/cliHelper.js
+++ tests/cypress/scripts/cliHelper.js
@@ -5,9 +5,9 @@
 
 export const cliHelper = {
     getTargetManagedCluster: () => {
-      return cy.exec('oc get managedclusters -o custom-columns=NAME:.metadata.name').then(result => {
+      return cy.exec('oc get managedclusters --field-selector=metadata.name!=local-cluster -o custom-columns=NAME:.metadata.name').then(result => {
         const managedClusters = result.stdout.split('\n').slice(1)
-        const targetCluster = managedClusters.find(c => !c.includes('local-cluster') && !c.includes('console-ui-test-') && c.includes('/^(|import)-\d\w+/g'))
+        const targetCluster = managedClusters.find(c => !c.includes('console-ui-test-') && c.includes('/^(|import)-\d\w+/g'))
         return cy.wrap(targetCluster)
       })
     }
