14bedd06ba8659690e94eda5377d72002a6f0d35
dislbenn <lavontae.bennett@gmail.com>
2021-03-10 13:05:09 -0500

    filter managed clusters for the import-xxx cluster

--- tests/cypress/scripts/cliHelper.js
+++ tests/cypress/scripts/cliHelper.js
@@ -7,7 +7,7 @@ export const cliHelper = {
     getTargetManagedCluster: () => {
       return cy.exec('oc get managedclusters -o custom-columns=NAME:.metadata.name').then(result => {
         const managedClusters = result.stdout.split('\n').slice(1)
-        const targetCluster = managedClusters.find(c => !c.includes('local-cluster') && !c.includes('console-ui-test-'))
+        const targetCluster = managedClusters.filter((cluster) => cluster.startsWith('import-'))
         return cy.wrap(targetCluster)
       })
     }
