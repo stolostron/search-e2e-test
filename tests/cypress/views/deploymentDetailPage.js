a88ff83c0dd4dc57d2f3038b893b2323fe1b2836
dislbenn <lavontae.bennett@gmail.com>
2021-03-19 14:43:27 -0400

    Update deploymentDetailPage.js

--- tests/cypress/views/deploymentDetailPage.js
+++ tests/cypress/views/deploymentDetailPage.js
@@ -9,7 +9,8 @@
 
 export const deploymentDetailPage = {
   whenScaleReplicasTo:(replicas) => {
-    cy.get('button.pf-m-primary').wait(1000).click({ timeout: 20000, force: true }).wait(1000)
+    cy.wait(2000) // DOM element tends to detach within cypress at this point. Adding a slight delay to avoid that scenario.
+    cy.get('button.pf-m-primary').click({ timeout: 20000, force: true }).wait(1000)
     cy.get('.react-monaco-editor-container').click().type(Cypress.platform !== 'darwin' ? '{ctrl}f' : '{meta}f')
       .get('.find-widget .monaco-inputbox textarea:first').focus().click().type('replicas: 1')
     cy.get('.react-monaco-editor-container .view-line > span')
