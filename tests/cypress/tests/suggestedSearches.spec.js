292787c0bb214426429db357a41d46a8d91a07b8
Disaiah Bennett <lavontae.bennett@gmail.com>
2021-03-19 19:02:06 -0400

    Update suggestedSearches.spec.js

--- tests/cypress/tests/suggestedSearches.spec.js
+++ tests/cypress/tests/suggestedSearches.spec.js
@@ -25,19 +25,16 @@ describe('Search: Verify the suggested search templates', function() {
   it(`[P3][Sev3][${squad}] should see the workloads template & search tag in search items`, function() {
     savedSearches.whenSelectCardWithTitle('Workloads')
     searchBar.shouldContainTag('kind:daemonset,deployment,job,statefulset,replicaset')
-    savedSearches.whenRelatedItemsExist()
   });
 
   it(`[P3][Sev3][${squad}] should see the unhealthy pods template & search tag in search items`, function() {
     savedSearches.whenSelectCardWithTitle('Unhealthy pods')
     searchBar.shouldContainTag('kind:pod')
     searchBar.shouldContainTag('status:Pending,Error,Failed,Terminating,ImagePullBackOff,CrashLoopBackOff,RunContainerError,ContainerCreating')
-    savedSearches.whenRelatedItemsExist()
   });
 
   it(`[P3][Sev3][${squad}] should see the created last hour template & search tag in search items`, function() {
     savedSearches.whenSelectCardWithTitle('Created last hour')
     searchBar.shouldContainTag('created:hour')
-    savedSearches.whenRelatedItemsExist()
   });
 })
