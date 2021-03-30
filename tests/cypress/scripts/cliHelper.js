0220fc7a7b65d25955afb9f77cc3f6fd17ed12f7
dislbenn <lavontae.bennett@gmail.com>
2021-03-30 11:15:15 -0400

    added fix to select imported cluster

--- tests/cypress/scripts/cliHelper.js
+++ tests/cypress/scripts/cliHelper.js
@@ -4,11 +4,23 @@
  ****************************************************************************** */
 
 export const cliHelper = {
-    getTargetManagedCluster: () => {
-      return cy.exec('oc get managedclusters -o custom-columns=NAME:.metadata.name').then(result => {
-        const managedClusters = result.stdout.split('\n').slice(1)
-        const targetCluster = managedClusters.find(c => !c.includes('local-cluster') && !c.includes('console-ui-test-'))
-        return cy.wrap(targetCluster)
-      })
-    }
+  getTargetManagedCluster: () => {
+    return cy.exec('oc get managedclusters -o custom-columns=NAME:.metadata.name').then(result => {
+      const managedClusters = result.stdout.split('\n').slice(1)
+      let targetCluster
+
+      // In the canary tests, we only need to focus on the import-xxxx managed cluster.
+      if (Cypress.env('NODE_ENV') !== 'development' && Cypress.env('NODE_ENV') !== 'debug') {
+        targetCluster = managedClusters.find((c) => c.startsWith('import-'))
+      }
+      
+      // When running locally or if the cluster is not available, try testing on an available managed cluster.
+      if (targetCluster === undefined) {
+        targetCluster = managedClusters.find((c) => !c.includes('local-cluster'))
+      }
+
+      cy.log(`Testing with Managed Cluster: ${targetCluster}`)
+      return cy.wrap(targetCluster)
+    })
   }
+}
