3f9ae69e579ca4c3bf39b018131a0b97f20d8682
anamdhar <anamdhar@redhat.com>
2021-04-07 10:42:20 +0530

    Removed Welcome page tests

--- tests/cypress/tests/RBAC_tests.spec.js
+++ tests/cypress/tests/RBAC_tests.spec.js
@@ -6,29 +6,17 @@
 /// <reference types="cypress" />
 
 import { squad } from '../config'
-import { welcomePage } from '../views/welcome'
 import { overviewPage } from '../views/overview'
 
 const rbac_users = ['search-e2e-admin-cluster', 'search-e2e-admin-ns', 'search-e2e-view-ns', 'search-e2e-edit-ns']
 const password = Cypress.env('OPTIONS_HUB_PASSWORD')
 const IDP = 'search-e2e-htpasswd'
 
-describe('RBAC users to read the Welcome and Overview page', function () {
-    const welcomePagePolarionIDs = ['729', '918', '922', '923']
+describe('RBAC users to read the Overview page', function () {
     const overviewPagePolarionIDs = ['731', '921', '919', '920']
     afterEach(function () {
         cy.logout()
     })
-    for (const [index, user] of rbac_users.entries())
-    {
-        var roleAccess = user.split('-')
-        it('RHACM4K-'+welcomePagePolarionIDs[index]+'[P1][Sev1]['+squad+'] As an user with name '+user+' with '+roleAccess[3]+'-role-binding of default '+roleAccess[2]+' role, the user can read the Welcome page.', function () {
-            cy.login(user, password, IDP)
-            welcomePage.whenGoToWelcomePage()
-            welcomePage.validateSvcs()
-            welcomePage.validateConnect()
-        })
-    }
     for (const [index, user] of rbac_users.entries())
     {
         var roleAccess = user.split('-')
