ac2517b09fb3fdce63d7e1f82a45e3d61f411a8c
Jorge Padilla <jpadilla@redhat.com>
2021-04-22 22:40:55 -0400

    Update index.js

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -34,7 +34,8 @@ before(() => {
 beforeEach(() => {
   Cypress.Cookies.preserveOnce('acm-access-token-cookie', '_oauth_proxy', 'XSRF-TOKEN', '_csrf')
   timeoutID = setTimeout(() => {
-    cy.task('log', `Test timed out. ${err}`)
+    Cypress.task('log', `Test timed out.`)
+    Cypress.task('log', `Test timed out with error: ${err}`)
     console.error(err)
     throw Error(err)
   }, 60000 * 5)
