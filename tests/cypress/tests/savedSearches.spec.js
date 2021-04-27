2a24f97111220457a889c3b18d879627506f6671
dislbenn <lavontae.bennett@gmail.com>
2021-04-27 16:44:10 -0400

    added login for savedsearches

--- tests/cypress/tests/savedSearches.spec.js
+++ tests/cypress/tests/savedSearches.spec.js
@@ -17,6 +17,10 @@ const queryOCMaEditedName = `[E2E] default namespace search - ${Date.now()}`
 const queryOcmaEditedDesc = '[Created by Search E2E automation] This is searching that each cluster should have default namespace -2'
 
 describe('RHACM4K-412 - Search: Saved searches', function() {
+  before(function() {
+    cy.login()
+  })
+
   after(function() {
     savedSearches.whenDeleteSavedSearch(queryDefaultNamespaceName)
     savedSearches.whenDeleteSavedSearch(queryOcmaNamespaceName)
