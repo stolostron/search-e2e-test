ce9e010582f009a3a7f9be4ad0e985109863fb7d
Disaiah Bennett <lavontae.bennett@gmail.com>
2021-05-03 13:40:18 -0400

    rbac test stabilize (#121)
    * updated RBAC_tests.spec.js
    
    * updated RBAC_tests.spec.js

--- tests/cypress/tests/RBAC_tests.spec.js
+++ tests/cypress/tests/RBAC_tests.spec.js
@@ -15,14 +15,21 @@ const IDP = 'search-e2e-htpasswd'
 describe('RBAC users to read the Overview page', function () {
     const overviewPagePolarionIDs = ['731', '921', '919', '920']
 
-    afterEach(function () {
+    afterEach(function() {
         cy.logout()
     })
 
     rbac_users.forEach((user, index) => {
         var roleAccess = user.split('-')
+        var skip = false
+
         it('RHACM4K-'+overviewPagePolarionIDs[index]+'[P1][Sev1]['+squad+'] As an user with name '+user+' with '+roleAccess[3]+'-role-binding of default '+roleAccess[2]+' role, the user can read the Overview page.', function () {
-            cy.login(user, password, IDP)
+            if (!skip) {
+                cy.login(user, password, IDP)
+                cy.log('Login successful: skipping login if test fails')
+                skip = true
+            }
+
             overviewPage.whenGoToOverviewPage()
             overviewPage.shouldLoad()
             overviewPage.shouldHaveLinkToSearchPage()
