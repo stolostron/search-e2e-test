a8d63f1a28c624d4d6449008b561e8b4feff5af1
dislbenn <lavontae.bennett@gmail.com>
2021-06-24 11:56:33 -0400

    updated rbac setup

--- build/rbac-setup.sh
+++ build/rbac-setup.sh
@@ -8,28 +8,28 @@
 #     Sets up cluster users and resources for RBAC tests
 # From the search-e2e-test project directory, invoke as:
 #     ./build/rbac-setup.sh
-#    
-
-set -e
 # DIR="$( find $HOME -type d -name "search-e2e-test" )"
 # using abosolute path, relative path is not working in travis
-RBAC_DIR=./tests/config/rbac_yaml
+
+set -e
+RBAC_DIR=${TRAVIS_BUILD_DIR:-.}/tests/config/rbac_yaml
+
 if [ ! -d ${RBAC_DIR} ]; then
   echo "Error: Directory ${RBAC_DIR} does not exist. Not creating RBAC resources."
   exit 1
 fi
 
-if [[ -z $OPTIONS_HUB_PASSWORD && -z $CYPRESS_OPTIONS_HUB_PASSWORD ]]; then
-  echo "Error: RBAC password not set in variable OPTIONS_HUB_PASSWORD or CYPRESS_OPTIONS_HUB_PASSWORD. The variables can be set within the options.yaml file or exported as an environment variable."
+if [[ -z ${OPTIONS_HUB_PASSWORD} && -z ${CYPRESS_OPTIONS_HUB_PASSWORD} ]]; then
+  echo "Error: RBAC password not set in variable OPTIONS_HUB_PASSWORD or CYPRESS_OPTIONS_HUB_PASSWORD."
   exit 1
 fi
 
-passwd="{$OPTIONS_HUB_PASSWORD:-$CYPRESS_OPTIONS_HUB_PASSWORD}"
+passwd=${OPTIONS_HUB_PASSWORD:-CYPRESS_OPTIONS_HUB_PASSWORD}
 
 if ! which htpasswd &>/dev/null; then
   if which apt-get &>/dev/null; then
-    apt-get update
-    apt-get install -y apache2-utils
+    sudo apt-get update
+    sudo apt-get install -y apache2-utils
   else
     echo "Error: Package manager apt-get not found. Failed to find or install htpasswd."
     exit 1
@@ -42,7 +42,9 @@ for access in cluster ns; do
     htpasswd -b ${RBAC_DIR}/htpasswd search-e2e-${role}-${access} ${passwd}
   done
 done
-oc create secret generic search-e2e-users --from-file=htpasswd=${RBAC_DIR}/htpasswd -n openshift-config || true
+
+set +e
+oc create secret generic search-e2e-users --from-file=htpasswd=${RBAC_DIR}/htpasswd -n openshift-config
 rm ${RBAC_DIR}/htpasswd
 if [[ -z "$(oc -n openshift-config get oauth cluster -o jsonpath='{.spec.identityProviders}')" ]]; then
   oc patch -n openshift-config oauth cluster --type json --patch '[{"op":"add","path":"/spec/identityProviders","value":[]}]'
