313172d56eace015949c538584c2ff4d32e23a0c
Atif Shafi <shafiatifbin@gmail.com>
2021-12-10 14:36:29 -0500

    Updated Jenkinsfile

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -32,6 +32,7 @@ const err = 'Test taking too long! It has been running for 5 minutes.'
 // https://multicloud-console.apps.ocp4-aws-3.dev09.red-chesterfield.com/
 
 const baseDomain = Cypress.env("CYPRESS_BASE_URL").replace("https://multicloud-console.apps.", "")
+cy.log("#######Base Domain: " + baseDomain)
 Cypress.env('CYPRESS_OPTIONS_HUB_BASEDOMAIN', baseDomain)
 
 
