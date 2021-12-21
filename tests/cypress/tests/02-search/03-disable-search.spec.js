1a291b9545d5223e1649e1aced24ea651de58292
Atif Shafi <shafiatifbin@gmail.com>
2021-12-21 16:52:13 -0500

    Added RHACM4K-2882 within an existing script

--- tests/cypress/tests/02-search/03-disable-search.spec.js
+++ tests/cypress/tests/02-search/03-disable-search.spec.js
@@ -6,7 +6,7 @@
 /// <reference types="cypress" />
 
 import {tags} from '../../config'
-import {searchPage} from '../../views/search'
+import {searchBar, searchPage} from '../../views/search'
 import {cliHelper} from "../../scripts/cliHelper";
 import {clustersPage} from "../../views/clusters";
 
@@ -14,8 +14,10 @@ describe('Search: Test "search-collector" add-on function', {tags: tags.componen
     before(() => {
         // Get name of an imported cluster
         cliHelper.getTargetManagedCluster().as('managedCluster')
-        if (this.managedCluster === 'local-cluster'){
-            Cypress.on('fail - No managed cluster found', (error, runnable) => { throw error; });
+        if (this.managedCluster === 'local-cluster') {
+            Cypress.on('fail - No managed cluster found', (error, runnable) => {
+                throw error;
+            });
         }
     })
 
@@ -45,6 +47,44 @@ describe('Search: Test "search-collector" add-on function', {tags: tags.componen
         /* Verify Search page */
         // Verify warning text does not exists
         searchPage.whenSearchDisabledOnClusters('false')
+    })
+
+    it(`RHACM4K-2882: search collector agent addon to reflect the status on hub post installed on managed cluster`, {tags: ['@RHACM4K-2882', '@post-release']}, function () {
+        /* Verify search-collector can be found on Add-ons page*/
+        // Log in yo ACM
+        cy.login()
+
+        /* Disable search-collector */
+        // Flag search-collector to false
+        cliHelper.flagSearchCollector('false', this.managedCluster)
+        // Go to 'Add-on' page of a managed cluster and verify 'search-collector' is not available
+        clustersPage.whenAddonAvailable('false', this.managedCluster, 'search-collector')
+
+        /* Verify Search page */
+        // Verify expected warning text exists
+        searchPage.whenSearchDisabledOnClusters('true')
+
+        /* Verify addon */
+        // Go to 'Add-on' page of a managed cluster and verify 'search-collector' is available
+        clustersPage.whenAddonAvailable('true', this.managedCluster, 'search-collector')
+        // Go to 'Add-on' page of local cluster and verify 'search-collector' is not available
+        clustersPage.whenAddonAvailable('false', 'local-cluster', 'search-collector')
+
+        /* Verify search-collector from search bar*/
+        // Enter cluster name
+        searchBar.whenFilterByCluster(this.managedCluster)
+        // Enter kind 'lease'
+        searchBar.whenFilterByKind('lease')
+        // Enter namespace 'open-cluster-management-agent-addon'
+        searchBar.whenFilterByNamespace('open-cluster-management-agent-addon')
+        // Enter name 'search-collector'
+        searchBar.whenFilterByName('search-collector')
+        // Go to details page
+        searchPage.whenGoToResourceDetailItemPage(
+            'Lease',
+            'search-collector'
+        )
+
     })
 })
 
