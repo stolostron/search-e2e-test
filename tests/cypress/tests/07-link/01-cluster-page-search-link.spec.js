d869f74c8ffab67a34e1b3bfa65b16842208366b
Atif Shafi <shafiatifbin@gmail.com>
2021-12-23 14:04:15 -0500

    Fixed functions for 2.4, added test steps for RHACM4K-413

--- tests/cypress/tests/07-link/01-cluster-page-search-link.spec.js
+++ tests/cypress/tests/07-link/01-cluster-page-search-link.spec.js
@@ -5,23 +5,35 @@
 
 /// <reference types="cypress" />
 
-import { squad, tags } from '../../config'
-import { clustersPage } from '../../views/clusters'
+import {squad, tags} from '../../config'
+import {clustersPage} from '../../views/clusters'
+import {overviewPage} from "../../views/overview";
+import {leftNav} from "../../views/welcome";
 
-describe('RHACM4K-413: Search: Linked page', { tags: [] }, function () {
-  context('prereq: user should log into the ACM console', { tags: tags.required }, function () {
-    it(`[P1][Sev1][${squad}] should login`, function () {
-      cy.login()
-    })
-  })
-
-  context('verify: cluster page link to search page', { tags: [] }, function () {
-    it(`[P1][Sev1][${squad}] should load the cluster page`, function () {
-      clustersPage.shouldLoad()
+describe('RHACM4K-413: Search: Linked page', {tags: []}, function () {
+    context('prereq: user should log into the ACM console', {tags: tags.required}, function () {
+        it(`[P1][Sev1][${squad}] should login`, function () {
+            cy.login()
+        })
     })
 
-    it(`[P2][Sev2][${squad}] clusters page should have link to search page`, function () {
-      clustersPage.shouldHaveLinkToSearchPage()
+    context('verify: cluster page link to search page', {tags: []}, function () {
+        // it(`[P1][Sev1][${squad}] should load the cluster page`, function () {
+        //     clustersPage.shouldLoad()
+        // })
+        //
+        // it(`[P2][Sev2][${squad}] clusters page should have link to search page`, function () {
+        //     clustersPage.shouldHaveLinkToSearchPage()
+        // })
+        //
+        it(`[P2][Sev2][${squad}] should load overview page`, function () {
+            // clustersPage.shouldHaveLinkToSearchPage()
+            leftNav.goToOverview()
+        })
+
+        it(`[P2][Sev2][${squad}] should load application page`, function () {
+            // clustersPage.shouldHaveLinkToSearchPage()
+            leftNav.goToApplications()
+        })
     })
-  })
 })
