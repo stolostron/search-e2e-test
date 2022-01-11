17c8eb146592f14487d47d510047e0f46b6179b0
Atif Shafi <shafiatifbin@gmail.com>
2022-01-11 14:23:51 -0500

    Updated RHACM4K-2882

--- tests/cypress/tests/02-search/03-disable-search.spec.js
+++ tests/cypress/tests/02-search/03-disable-search.spec.js
@@ -22,7 +22,6 @@ describe('Search: Test "search-collector" add-on function', {tags: [tags.compone
             });
         }
     })
-
     // Reset 'search' component
     after(function () {
         // Flag search-collector to true
@@ -30,6 +29,8 @@ describe('Search: Test "search-collector" add-on function', {tags: [tags.compone
     })
 
     it(`RHACM4K-3941: Search function can be disabled on the managed cluster`, {tags: ['@RHACM4K-3941', '@post-release']}, function () {
+        // Flag search-collector to true
+        cliHelper.flagSearchCollector('true', this.managedCluster)
         /* Verify search-collector can be found on Add-ons page*/
         // Go to 'Add-on' page of a managed cluster and verify 'search-collector' is available
         clustersPage.whenAddonAvailable('true', this.managedCluster, 'search-collector')
@@ -56,8 +57,6 @@ describe('Search: Test "search-collector" add-on function', {tags: [tags.compone
     })
 
     it(`RHACM4K-2882: search collector agent addon to reflect the status on hub post installed on managed cluster`, {tags: ['@RHACM4K-2882', '@post-release']}, function () {
-        /* Verify search-collector can be found on Add-ons page*/
-
         /* Disable search-collector */
         // Flag search-collector to false
         cliHelper.flagSearchCollector('false', this.managedCluster)
