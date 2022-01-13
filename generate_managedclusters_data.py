925ebceb37ffa208b885a7a7a7c32921093dfd7a
Atif Shafi <shafiatifbin@gmail.com>
2022-01-13 16:45:38 -0500

    Fixed Jenkinsfile

--- generate_managedclusters_data.py
+++ generate_managedclusters_data.py
@@ -12,21 +12,25 @@ cluster_data={"managedClusters":[]}
 oc_command = ['oc', 'get', 'managedclusters', '--selector', 'name!=local-cluster','-o', 'json']
 managed_clusters = json.loads(run_command(oc_command))
 
+list_index=0
+
 for index,item in enumerate(managed_clusters['items']):
     cluster_status = "Unknown"
     for condition in item['status']['conditions']:
         if condition['type'] == 'ManagedClusterConditionAvailable':
             cluster_status = condition['status']
     if cluster_status == "True":
-        cluster_data["managedClusters"].append({"name" : item['metadata']['name']})
         if item['metadata']['annotations']["open-cluster-management/created-via"] == "hive":
-            cluster_data["managedClusters"][index]["base_domain"] = item['spec']['managedClusterClientConfigs'][0]['url'][12:].split(':')[0]
+            cluster_data["managedClusters"].append({"name" : item['metadata']['name']})
+            cluster_data["managedClusters"][list_index]["api_url"] = item['spec']['managedClusterClientConfigs'][0]['url']
+            cluster_data["managedClusters"][list_index]["base_domain"] = item['spec']['managedClusterClientConfigs'][0]['url'].split(item['metadata']['name'])[-1].split(":")[0][1:]
             secret_command = ['oc', 'get', 'secrets','--selector=hive.openshift.io/secret-type=kubeadmincreds', '-o', 'json', '-n']
             secret_command.append(item['metadata']['name'])
             secret_list = json.loads(run_command(secret_command))
             password = base64.b64decode(secret_list['items'][0]['data']['password']).decode('utf-8')
-            cluster_data["managedClusters"][index]["username"] = 'kubeadmin'
-            cluster_data["managedClusters"][index]["password"] = password
+            cluster_data["managedClusters"][list_index]["username"] = 'kubeadmin'
+            cluster_data["managedClusters"][list_index]["password"] = password
+            list_index += 1
 
 with open('managedClusters.json', 'w') as f:
     json.dump(cluster_data, f)
