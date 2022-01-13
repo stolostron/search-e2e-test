398c5f2fe160c02c8c7552c3fc525cdf409098da
Atif Shafi <shafiatifbin@gmail.com>
2022-01-13 15:14:28 -0500

    Fixed Jenkinsfile

--- generate_managedclusters_data.py
+++ generate_managedclusters_data.py
@@ -19,6 +19,8 @@ for index,item in enumerate(managed_clusters['items']):
             cluster_status = condition['status']
     if cluster_status == "True":
         cluster_data["managedClusters"].append({"name" : item['metadata']['name']})
+        print(item['spec']['managedClusterClientConfigs'])
+        print('---')
         print(item['spec']['managedClusterClientConfigs'][0]['url'])
         cluster_data["managedClusters"][index]["base_domain"] = item['spec']['managedClusterClientConfigs'][0]['url'][12:].split(':')[0]
         secret_command = ['oc', 'get', 'secrets','--selector=hive.openshift.io/secret-type=kubeadmincreds', '-o', 'json', '-n']
