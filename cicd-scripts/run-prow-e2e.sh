e248ccd0c804e7eb2a3d94bc36b6002360710e74
dislbenn <lavontae.bennett@gmail.com>
2022-01-14 18:17:58 -0500

    updated run-prow-e2e.sh
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- cicd-scripts/run-prow-e2e.sh
+++ cicd-scripts/run-prow-e2e.sh
@@ -2,15 +2,20 @@
 # Copyright (c) 2021 Red Hat, Inc.
 # Copyright Contributors to the Open Cluster Management project
 
-set -e
+###############################################################################
+# Test Setup
+###############################################################################
+
+echo -e "Shared dir: $SHARED_DIR\n"
 
 # Test env
-BROWSER=chrome
-
-export OPTIONS_HUB_KUBECONFIG="${SHARED_DIR}/hub-1.kc"
-export OPTIONS_MANAGED_KUBECONFIG="${SHARED_DIR}/managed-1.kc"
-
-echo -e "$SHARED_DIR\n"
+export BROWSER=chrome
+export OPTIONS_HUB_KUBECONFIG=${SHARED_DIR}/hub-1.kc
+export OPTIONS_KUBECONFIG_MOUNT_PATH=${SHARED_DIR}/managed-1.kc
+export OPTIONS_MANAGED_KUBECONFIG=${OPTIONS_KUBECONFIG_MOUNT_PATH}
+export SKIP_API_TEST=true
+export SKIP_UI_TEST=false
+export TEST_MODE=BVT
 
 # Hub cluster
 HUB_CREDS=$(cat ${SHARED_DIR}/hub-1.json)
@@ -30,14 +35,10 @@ export OPTIONS_MANAGED_BASEDOMAIN=$(echo $MANAGED_CREDS | jq -r '.api_url')
 export OPTIONS_MANAGED_USER=$(echo $MANAGED_CREDS | jq -r '.username')
 export OPTIONS_MANAGED_PASSWORD=$(echo $MANAGED_CREDS | jq -r '.password')
 
-# Env variables
-export SKIP_API_TEST=true
-export TEST_MODE=BVT
-
 env | grep "OPTIONS"
 
 echo -e "\nRunning Search E2E tests in ${CYPRESS_TEST_MODE} test mode."
 
 # npm run test
 
-exit 0
+# exit 0
