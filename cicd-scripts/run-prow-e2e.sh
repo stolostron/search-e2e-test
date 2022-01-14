3323b196ad70714086559ebc6926b368ba4efd11
dislbenn <lavontae.bennett@gmail.com>
2022-01-14 17:15:59 -0500

    updated makefile.prow
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- cicd-scripts/run-prow-e2e.sh
+++ cicd-scripts/run-prow-e2e.sh
@@ -6,7 +6,21 @@ set -e
 
 echo -e "Shared Directory: $SHARED_DIR\n"
 
+cat /tmp/secret/hub-1.json
+
+# Env variables
+export SKIP_API_TEST=true
+
+export OPTIONS_HUB_BASEDOMAIN=$(echo $HUB_CREDS | jq -r '.api_url')
+export OPTIONS_HUB_USER=$(echo $HUB_CREDS | jq -r '.username')
+export OPTIONS_HUB_PASSWORD=$(echo $HUB_CREDS | jq -r '.password')
+
 # Cypress env variables
 export CYPRESS_TEST_MODE=BVT
 
+echo -e "Environment Variables"
+env
+
 echo -e "Running Search E2E tests in ${CYPRESS_TEST_MODE} test mode."
+
+# npm run test
