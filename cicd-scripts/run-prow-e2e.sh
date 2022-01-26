49255dc860d9807c9488e72e804da16143687a91
dislbenn <lavontae.bennett@gmail.com>
2022-01-26 14:41:14 -0500

    updated run-prow-e2e.sh
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- cicd-scripts/run-prow-e2e.sh
+++ cicd-scripts/run-prow-e2e.sh
@@ -20,19 +20,16 @@ export TEST_MODE=BVT
 
 # Hub cluster
 HUB_CREDS=$(cat ${SHARED_DIR}/hub-1.json)
-OPTIONS_HUB_CONSOLE_NAME=$(echo $HUB_CREDS | jq -r '.console_url')
-
+export OPTIONS_HUB_CONSOLE_NAME=$(yq e '.console_url' $SHARED_DIR/hub-1.json)
 export OPTIONS_HUB_BASEDOMAIN=${OPTIONS_HUB_CONSOLE_NAME:39}
-# export OPTIONS_HUB_BASEDOMAIN=$(echo $HUB_CREDS | jq -r '.api_url')
-export OPTIONS_HUB_USER=$(echo $HUB_CREDS | jq -r '.username')
-export OPTIONS_HUB_PASSWORD=$(echo $HUB_CREDS | jq -r '.password')
+export OPTIONS_HUB_USER=$(yq e '.username' $SHARED_DIR/hub-1.json)
+export OPTIONS_HUB_PASSWORD=$(yq e '.password' $SHARED_DIR/hub-1.json)
 
 # Managed cluster
 MANAGED_CREDS=$(cat ${SHARED_DIR}/managed-1.json)
-
-export OPTIONS_MANAGED_BASEDOMAIN=$(echo $MANAGED_CREDS | jq -r '.api_url')
-export OPTIONS_MANAGED_USER=$(echo $MANAGED_CREDS | jq -r '.username')
-export OPTIONS_MANAGED_PASSWORD=$(echo $MANAGED_CREDS | jq -r '.password')
+export OPTIONS_MANAGED_BASEDOMAIN=$(yq e '.api_url' $SHARED_DIR/managed-1.json)
+export OPTIONS_MANAGED_USER=$(yq e '.username' $SHARED_DIR/managed-1.json)
+export OPTIONS_MANAGED_PASSWORD=$(yq e '.password' $SHARED_DIR/managed-1.json)
 
 echo -e "\nRunning Search E2E tests in ${CYPRESS_TEST_MODE} test mode."
 ./start-tests.sh
