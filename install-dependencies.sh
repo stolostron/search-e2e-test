f6f23ee57de44f774bb1d2f1e9ac4d5ee238801e
dislbenn <lavontae.bennett@gmail.com>
2022-02-04 14:01:53 -0500

    code change suggestions
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,7 +5,7 @@
 mkdir clis-unpacked
 
 # Install OpenShift and Kubectl CLI.
-echo "Installing oc and kubectl clis..."
+echo 'Installing oc and kubectl clis...'
 curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.6.4/openshift-client-linux-4.6.4.tar.gz
 tar -xzf oc.tar.gz -C clis-unpacked
 chmod 755 ./clis-unpacked/oc
@@ -13,7 +13,10 @@ chmod 755 ./clis-unpacked/kubectl
 mv ./clis-unpacked/oc /usr/local/bin/oc
 mv ./clis-unpacked/kubectl /usr/local/bin/kubectl
 
+echo -e 'oc and kubectl cli install completed.'
+
 # Install htpasswd utility
+echo 'Installing htpasswd utility...'
 apt-get update && apt-get install -y apache2-utils
 
-echo 'oc and kubectl cli install completed.'
+echo 'htpasswd utilities install completed.'
