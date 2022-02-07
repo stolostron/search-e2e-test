63b01903ccd5b498339651cbc4490f72f4f0a47c
dislbenn <lavontae.bennett@gmail.com>
2022-02-07 14:40:21 -0500

    added error flag back to rbac setup
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- build/rbac-setup.sh
+++ build/rbac-setup.sh
@@ -11,6 +11,8 @@
 # DIR="$( find $HOME -type d -name "search-e2e-test" )"
 # using abosolute path, relative path is not working in travis
 
+set -e
+
 RBAC_DIR=${TRAVIS_BUILD_DIR:-.}/tests/config/rbac_yaml
 
 if [ ! -d ${RBAC_DIR} ]; then
