4624d524d24b43223361130fd45b3683045e7765
Disaiah Bennett <lavontae.bennett@gmail.com>
2022-02-08 12:24:39 -0500

    [release-2.5] Updated file formatting with prettier (#181)
    * run prettier to fix format
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>
    
    * added prow scripts
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>
    
    * updated prow makefile
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>
    
    * updated dockerfile
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>
    
    * code change suggestions
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>
    
    * updated makefile.prow
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>
    
    * updated prow e2e
    
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- tests/cypress/tests/02-search/01-search.spec.js
+++ tests/cypress/tests/02-search/01-search.spec.js
@@ -15,14 +15,18 @@ const clusterModes = [
     valueFn: () => cy.wrap('local-cluster'),
     skip: false,
     namespace: cliHelper.generateNamespace(),
-    kubeconfig: Cypress.env('USE_HUB_KUBECONFIG') ? `KUBECONFIG=${Cypress.env('OPTIONS_HUB_KUBECONFIG')}` : ''
+    kubeconfig: Cypress.env('USE_HUB_KUBECONFIG')
+      ? `KUBECONFIG=${Cypress.env('OPTIONS_HUB_KUBECONFIG')}`
+      : '',
   },
   {
     label: 'Managed',
     valueFn: () => cliHelper.getTargetManagedCluster(),
     skip: Cypress.env('SKIP_MANAGED_CLUSTER_TEST'),
     namespace: cliHelper.generateNamespace('', `managed-${Date.now()}`),
-    kubeconfig: Cypress.env('USE_MANAGED_KUBECONFIG') ? `KUBECONFIG=${Cypress.env('OPTIONS_MANAGED_KUBECONFIG')}` : ''
+    kubeconfig: Cypress.env('USE_MANAGED_KUBECONFIG')
+      ? `KUBECONFIG=${Cypress.env('OPTIONS_MANAGED_KUBECONFIG')}`
+      : '',
   },
 ]
 
@@ -34,61 +38,76 @@ clusterModes.forEach((clusterMode) => {
     return
   }
 
-  describe('Search: Search in ' + clusterMode.label + ' Cluster', { tags: tags.env }, function () {
-    before(function () {
-      clusterMode.valueFn().as('clusterName')
-    })
+  describe(
+    'Search: Search in ' + clusterMode.label + ' Cluster',
+    { tags: tags.env },
+    function () {
+      before(function () {
+        clusterMode.valueFn().as('clusterName')
+      })
 
-    // Log into cluster to clean up resources.
-    after(function () {
-      if (clusterMode.label === 'Managed' && Cypress.env('USE_MANAGED_KUBECONFIG')) {
-        cy.log('Skipping login and using import-kubeconfig file')
-      } else {
-        // Log into cluster with oc command.
-        cliHelper.login(clusterMode.label)
+      // Log into cluster to clean up resources.
+      after(function () {
+        if (
+          clusterMode.label === 'Managed' &&
+          Cypress.env('USE_MANAGED_KUBECONFIG')
+        ) {
+          cy.log('Skipping login and using import-kubeconfig file')
+        } else {
+          // Log into cluster with oc command.
+          cliHelper.login(clusterMode.label)
+        }
+        cliHelper.deleteNamespace(clusterMode.namespace, clusterMode.kubeconfig)
+      })
+
+      // Logging into the hub cluster UI.
+      if (clusterMode.label !== 'Managed') {
+        context(
+          'prereq: user should log into the ACM console',
+          { tags: tags.required },
+          function () {
+            it(`[P1][Sev1][${squad}] should login`, function () {
+              cy.login()
+            })
+          }
+        )
       }
-      cliHelper.deleteNamespace(clusterMode.namespace, clusterMode.kubeconfig)
-    })
 
-    // Logging into the hub cluster UI.
-    if (clusterMode.label !== 'Managed') {
-      context('prereq: user should log into the ACM console', { tags: tags.required }, function () {
-        it(`[P1][Sev1][${squad}] should login`, function () {
-          cy.login()
-        })
-      })
+      context(
+        'search resources: verify resource exist after creation',
+        { tags: tags.modes },
+        function () {
+          beforeEach(function () {
+            searchPage.whenGoToSearchPage()
+            searchBar.whenFilterByNamespace(clusterMode.namespace)
+            searchBar.whenFilterByCluster(this.clusterName)
+            searchPage.shouldLoadResults()
+          })
+
+          it(`[P3][Sev3][${squad}] should have expected count of relationships`, function () {
+            searchPage.whenExpandRelationshipTiles()
+            searchPage.shouldFindRelationshipTile('cluster', '1')
+            searchPage.shouldFindRelationshipTile('deployment', '1')
+            searchPage.shouldFindRelationshipTile('pod', '1')
+          })
+
+          it(`[P1][Sev1][${squad}] should work kind filter for deployment`, function () {
+            searchBar.whenFilterByKind('deployment')
+            searchPage.shouldFindResourceDetailItem(
+              'deployment',
+              clusterMode.namespace + '-deployment'
+            )
+          })
+
+          it(`[P1][Sev1][${squad}] should work kind filter for pod`, function () {
+            searchBar.whenFilterByKind('pod')
+            searchPage.shouldFindResourceDetailItem(
+              'pod',
+              clusterMode.namespace + '-deployment-'
+            )
+          })
+        }
+      )
     }
-
-    context('search resources: verify resource exist after creation', { tags: tags.modes },function () {
-      beforeEach(function () {
-        searchPage.whenGoToSearchPage()
-        searchBar.whenFilterByNamespace(clusterMode.namespace)
-        searchBar.whenFilterByCluster(this.clusterName)
-        searchPage.shouldLoadResults()
-      })
-
-      it(`[P3][Sev3][${squad}] should have expected count of relationships`, function () {
-        searchPage.whenExpandRelationshipTiles()
-        searchPage.shouldFindRelationshipTile('cluster', '1')
-        searchPage.shouldFindRelationshipTile('deployment', '1')
-        searchPage.shouldFindRelationshipTile('pod', '1')
-      })
-
-      it(`[P1][Sev1][${squad}] should work kind filter for deployment`, function () {
-        searchBar.whenFilterByKind('deployment')
-        searchPage.shouldFindResourceDetailItem(
-          'deployment',
-          clusterMode.namespace + '-deployment'
-        )
-      })
-
-      it(`[P1][Sev1][${squad}] should work kind filter for pod`, function () {
-        searchBar.whenFilterByKind('pod')
-        searchPage.shouldFindResourceDetailItem(
-          'pod',
-          clusterMode.namespace + '-deployment-'
-        )
-      })
-    })
-  })
+  )
 })
