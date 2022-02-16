9b5932b1a3e30b3940a0af28af47bda0f7f87c1b
dislbenn <lavontae.bennett@gmail.com>
2022-02-16 17:18:49 -0500

    re-enabled search pod logs test
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- tests/cypress/tests/02-search/02-pod-logs.spec.js
+++ tests/cypress/tests/02-search/02-pod-logs.spec.js
@@ -23,7 +23,7 @@ const clusterModes = [
   {
     label: 'Managed',
     valueFn: () => cliHelper.getTargetManagedCluster(),
-    skip: true, // Disabling the managed cluster tests until the console squad patches the issue for the logs action.
+  skip: Cypress.env('SKIP_MANAGED_CLUSTER_TEST'),
     namespace: cliHelper.generateNamespace('', `managed-${Date.now()}`),
     kubeconfig: Cypress.env('USE_MANAGED_KUBECONFIG')
       ? `KUBECONFIG=${Cypress.env('OPTIONS_MANAGED_KUBECONFIG')}`
@@ -59,10 +59,6 @@ clusterModes.forEach((clusterMode) => {
           cliHelper.login(clusterMode.label)
         }
         cliHelper.deleteNamespace(clusterMode.namespace, clusterMode.kubeconfig)
-
-        cy.log(
-          'Disabling managed cluster test. The console API is fixing the managed cluster logs, which will allow us to re-enable the cluster test.'
-        )
       })
 
       // Logging into the hub cluster UI.
