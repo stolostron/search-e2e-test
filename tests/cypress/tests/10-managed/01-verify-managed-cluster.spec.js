8248ac4b85eafa19a8b28ea887650dd07de17a26
dislbenn <lavontae.bennett@gmail.com>
2022-04-08 14:46:44 -0400

    updated managed cluster test
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- tests/cypress/tests/10-managed/01-verify-managed-cluster.spec.js
+++ tests/cypress/tests/10-managed/01-verify-managed-cluster.spec.js
@@ -7,7 +7,7 @@
 
 import { squad, tags } from '../../config'
 import { cliHelper } from '../../scripts/cliHelper'
-import { searchPage } from '../../views/search'
+import { searchBar, searchPage } from '../../views/search'
 
 describe(
   'RHACM4K-912: Search: Verify the managed cluster info in the search page',
@@ -23,23 +23,28 @@ describe(
       searchPage.whenGoToSearchPage()
     })
 
+    it(`[P3][Sev3][${squad}] should validate the endpoint version for the managed clusters are accurate`, function () {
+      searchPage.shouldValidateManagedCluster()
+    })
+
     context(
       'verify: managed cluster resource endpoint',
       { tags: tags.modes },
       () => {
-        it(`[P3][Sev3][${squad}] should validate the endpoint version for the managed clusters are accurate`, function () {
-          searchPage.shouldValidateManagedCluster()
+
+        beforeEach(function() {
+          searchPage.shouldFindKindInCluster('pod', this.clusterName)
+          searchBar.whenEnterTextInSearchBar('namespace')
+          searchPage.shouldSelectFirstSuggestionValue()
         })
 
         it(`[P3][Sev3][${squad}] should verify endpoint pods are all in running state`, function () {
-          searchPage.shouldVerifyManagedClusterPodsAreRunning(this.clusterName)
+          searchPage.shouldVerifyManagedClusterPodsAreRunning()
         })
 
-        it(`[P3][Sev3][${squad}] should verify the yaml information is correct and there are no errors in the logs`, function () {
-          searchPage.shouldVerifyManagedClusterPodsAreRunning(
-            this.clusterName,
-            true
-          )
+        it.only(`[P3][Sev3][${squad}] should verify the yaml information is correct and there are no errors in the logs`, function () {
+          searchPage.shouldVerifyManagedClusterPodsAreRunning()
+          searchPage.shouldVerifyPodsLogsInResourceTable()
         })
       }
     )
