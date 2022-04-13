67f91116303d7d000c79ffd91c83aa53a336d96f
dislbenn <lavontae.bennett@gmail.com>
2022-04-13 19:17:58 -0400

    refactored e2e test methods
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- tests/common-lib/sleep.js
+++ tests/common-lib/sleep.js
@@ -1,7 +1,10 @@
 // Copyright (c) 2020 Red Hat, Inc.
 
-const sleep = (milliseconds) => {
+/**
+ * Pauses the application and sleeps until the end of the specified interval.
+ * @param {int} milliseconds The amount of time to sleep in milliseconds. By default, this will sleep for 5 seconds.
+ * @returns {Promise} Returns the promise that will be fulfilled when the timeout has been set for the specified interval.
+ */
+const sleep = (milliseconds = 5000) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds))
 }
-
-exports.sleep = sleep
