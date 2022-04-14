8e024eed437242a3017395a6924115a4c8ee2792
dislbenn <lavontae.bennett@gmail.com>
2022-04-14 16:27:24 -0400

    updated search metadata to observability
    Signed-off-by: dislbenn <lavontae.bennett@gmail.com>

--- tests/cypress/config/index.js
+++ tests/cypress/config/index.js
@@ -6,7 +6,7 @@
 /**
  * Squad label displayed within the test metadata (Required by CICD).
  */
-export const squad = 'observability-usa'
+export const squad = 'observability'
 
 /**
  * Tags to determine the test environment.
