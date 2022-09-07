36dae8fc13931c2d01dfbfe6f17c8c64f60beb7d
Jorge Padilla <jpadilla@redhat.com>
2022-09-06 22:40:37 -0400

    Revert change to cypress config
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- tests/cypress/config/index.js
+++ tests/cypress/config/index.js
@@ -6,7 +6,7 @@
 /**
  * Squad label displayed within the test metadata (Required by CICD).
  */
-export const squad = require('../../config').get('squadName')
+const squad = 'observability-usa'
 
 /**
  * Tags to determine the test environment.
