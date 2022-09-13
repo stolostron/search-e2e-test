57a779eca8e2ca00d36f3f162e86479a83658412
Zack Layne <zlayne@redhat.com>
2022-09-13 15:59:51 -0400

    Remove search result validation from suggested search tests
    Signed-off-by: Zack Layne <zlayne@redhat.com>

--- tests/cypress/tests/suggested-searches.spec.js
+++ tests/cypress/tests/suggested-searches.spec.js
@@ -19,7 +19,6 @@ describe('RHACM4K-411: Search: Verify the suggested search templates', { tags: t
     it(`[P3][Sev3][${squad}] should see the workloads template & search tag in search items`, function () {
       suggestedSearches.whenSelectCardWithTitle('Workloads')
       searchBar.shouldContainTag('kind:daemonset,deployment,job,statefulset,replicaset')
-      suggestedSearches.whenVerifyRelatedItemsDetails()
     })
 
     it(`[P3][Sev3][${squad}] should see the unhealthy pods template & search tag in search items`, function () {
@@ -28,13 +27,11 @@ describe('RHACM4K-411: Search: Verify the suggested search templates', { tags: t
       searchBar.shouldContainTag(
         'status:Pending,Error,Failed,Terminating,ImagePullBackOff,CrashLoopBackOff,RunContainerError,ContainerCreating'
       )
-      suggestedSearches.whenVerifyRelatedItemsDetails()
     })
 
     it(`[P3][Sev3][${squad}] should see the created last hour template & search tag in search items`, function () {
       suggestedSearches.whenSelectCardWithTitle('Created last hour')
       searchBar.shouldContainTag('created:hour')
-      suggestedSearches.whenVerifyRelatedItemsDetails()
     })
   })
 })
