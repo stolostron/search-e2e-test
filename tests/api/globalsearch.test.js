684ef6ac906e4b5fb8f600d5e7b4f452360513ae
Sherin Varughese <shvarugh@redhat.com>
2023-09-15 16:54:17 -0400

    add globalSearch test
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

---
+++ tests/api/globalsearch.test.js
@@ -0,0 +1,70 @@
+// Copyright Contributors to the Open Cluster Management project
+
+jest.retryTimes(global.retry, { logErrorsBeforeRetry: true })
+
+const squad = require('../../config').get('squadName')
+const SEARCH_API_V1 = require('../../config').get('SEARCH_API_V1')
+const { getUserContext, getSearchApiRoute } = require('../common-lib/clusterAccess')
+const { ValidateSearchData, validationTimeout } = require('../common-lib/validateSearchData')
+const { resolveSearchItems } = require('../common-lib/searchClient')
+const { execSync } = require('child_process')
+const { execCliCmdString, expectCli } = require('../common-lib/cliClient')
+const { sleep } = require('../common-lib/sleep')
+
+const ns = 'search-rbac'
+const [usr0] = ['global-search-user']
+
+describe(`[P2][Sev2][${squad}] Search API: Verify RBAC`, () => {
+  beforeAll(async () => {
+    // Using ServiceAccounts for rbac tests because configuration is simpler.
+
+    const setupCmds = `
+    # export ns=search-rbac; export usr0=u0;
+    oc create namespace ${ns}
+    oc create serviceaccount ${usr0} -n ${ns}
+    oc create clusterrole ${usr0} --verb=get --resource=searches,searches/allManagedData --apigroups=search.open-cluster-management.io
+    oc create clusterrolebinding ${usr0} --clusterrole=${usr0} --serviceaccount=${ns}:${usr0}
+  
+
+    oc create configmap cm0 -n ${ns} --from-literal=key=cm0
+    oc create configmap cm1 -n ${ns} --from-literal=key=cm1`
+
+    // Run setup steps in parallel.
+    // - Create a route to access the Search API.
+    // - Create users and objects for this test.
+    const [route] = await Promise.all([getSearchApiRoute(), execCliCmdString(setupCmds)])
+    searchApiRoute = route
+
+    await sleep(10000) // Wait for service account and the search index to get updated.
+  }, 60000)
+
+  afterAll(async () => {
+    const teardownCmds = `
+    # export ns=search-rbac; export usr0=u0
+    oc delete ns ${ns}
+    oc delete clusterrolebinding ${usr0}
+    oc delete clusterrole ${usr0}`
+
+    await execCliCmdString(teardownCmds)
+  }, 10000)
+
+  describe(`with user ${usr0} (authorization to get searches)`, () => {
+    beforeAll(async () => {
+      user = await getUserContext({ usr: usr0, ns })
+    })
+
+    test('should validate RBAC configuration for user', () => {
+      expectCli(`oc auth can-i get searches --as=${user.fullName}`).not.toThrow()
+      expectCli(`oc auth can-i list configmap --as=${user.fullName}`).toThrow()
+    })
+
+    test('should not receive ConfigMap', () => ValidateSearchData({ user, kind: 'configmap' }), validationTimeout)
+ 
+    test(`should not match any resources containing the keyword 'cm0' or 'cm1`, async () => {
+      const items = await resolveSearchItems(user.token, { keywords: ['cm0', 'cm1'] })
+      expect(items).toHaveLength(0)
+      expect(items).toEqual([])
+    })
+})
+})
+
