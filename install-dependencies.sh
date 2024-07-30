147e2b530f3d32b09922e22bb275434ba667355d
zlayne <zlayne@redhat.com>
2024-07-30 19:05:12 -0400

    revert oc version
    Signed-off-by: zlayne <zlayne@redhat.com>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,12 +6,13 @@ mkdir clis-unpacked
 
 # Install OpenShift and Kubectl CLI.
 echo 'Installing oc and kubectl clis...'
-curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-client-linux.tar.gz
+curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.11.3/openshift-client-linux-4.11.3.tar.gz
 tar -xzf oc.tar.gz -C clis-unpacked
 chmod 755 ./clis-unpacked/oc
 chmod 755 ./clis-unpacked/kubectl
 mv ./clis-unpacked/oc /usr/local/bin/oc
 mv ./clis-unpacked/kubectl /usr/local/bin/kubectl
+rm -rf ./clis-unpacked
 
 echo -e 'oc and kubectl cli install completed.'
 
