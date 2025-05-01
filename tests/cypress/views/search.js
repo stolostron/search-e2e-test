479428e4b408d57a99ab4440f77d42e758f87ccc
Spencer McAvey <smcavey@redhat.com>
2025-05-01 16:11:22 -0400

    fix a test failure maybe
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>

--- tests/cypress/views/search.js
+++ tests/cypress/views/search.js
@@ -150,7 +150,7 @@ export const searchPage = {
    * Verify that the saved search tab is rendered on the Search page.
    */
   shouldRenderSavedSearchesTab: () => {
-    cy.get('.pf-v5-c-dropdown__toggle-text').filter(':contains(Saved searches)').should('exist')
+    cy.get('.pf-v5-c-dropdown__toggle-text').contains('Saved searches').should('exist')
   },
   /**
    * Verify that the search bar is rendered on the Search page.
