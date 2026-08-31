0d55405d2df12f8d0383c35000749df0cfa9fd11
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2026-08-31 18:10:53 +0200

    ACM-37751 - automation networkpolicy (#480)
    Signed-off-by: Spencer McAvey <smcavey@redhat.com>
    Co-authored-by: Spencer McAvey <smcavey@redhat.com>

--- tests/api/globalTeardown.js
+++ tests/api/globalTeardown.js
@@ -4,6 +4,8 @@ const { execSync } = require('child_process')
 
 module.exports = () => {
   const namespace = execSync(`oc get mch -A -o jsonpath='{.items[0].metadata.namespace}'`).toString()
+  execSync(`oc delete networkpolicy search-api-e2e-route-access -n ${namespace} --ignore-not-found`)
+  console.log('globalTeardown.js - Deleted NetworkPolicy search-api-e2e-route-access.')
   execSync(`oc delete route search-api-automation -n ${namespace} --ignore-not-found`)
   console.log('globalTeardown.js - Deleted route search-api-automation.')
 }
